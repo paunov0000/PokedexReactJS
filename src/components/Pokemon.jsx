9edd6c601d100f02dbd172d523e8948657ad1a87
paunov0000 <legendarygriefer96@gmail.com>
2024-03-21 02:55:49 +0200

    Made the CreatePokemon component fetch data from the api, have to learn about the hooks

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,13 +1,43 @@
-function CreatePokemonCard({ pokemon }) {
+import { useState, useEffect } from "react";
+
+function CreatePokemon() {
+  const [pokemonList, setPokemonList] = useState([]);
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=10")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.results);
+        setPokemonList(data.results);
+      });
+  }, []);
+
   return (
-      <div className="flex flex-col items-center w-28 p-3 border-solid border-2">
-        <p className="w-full text-right">#{pokemon.id}</p>
-        <div className="w-14 flex">
-          <img className="w-full" src={pokemon.image} alt={pokemon.name} />
-        </div>
-        <h2>{pokemon.name}</h2>
-      </div>
+    <div>
+      {pokemonList.map((pokemon) => {
+        return (
+          <div className="flex flex-col items-center w-28 p-3 border-solid border-2">
+            <div>
+              <p className="w-full text-right">#{pokemon.id}</p>
+              <div className="w-14 flex">
+                <img
+                  className="w-full"
+                  src={pokemon.image}
+                  alt={pokemon.name}
+                />
+              </div>
+              <h2>{pokemon.name}</h2>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+    //   <p className="w-full text-right">#{pokemon.id}</p>
+    //   <div className="w-14 flex">
+    //     <img className="w-full" src={pokemon.image} alt={pokemon.name} />
+    //   </div>
+    //   <h2>{pokemon.name}</h2>
   );
 }
 
-export default CreatePokemonCard;
+export default CreatePokemon;
