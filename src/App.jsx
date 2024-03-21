473083363e317a802b9ad975be1ff95f57535593
paunov0000 <legendarygriefer96@gmail.com>
2024-03-21 02:57:09 +0200

    Used the CreatePokemon component, removed the old one, also seemed way too off

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import { useState } from 'react'
-import './App.css'
-import CreatePokemonCard from './components/Pokemon'
+import { useEffect, useState } from "react";
+import "./App.css";
+import CreatePokemon from "./components/Pokemon.jsx";
 
 function App() {
   // const [count, setCount] = useState(0)
   const pokemonTest = {
     id: 4,
-    name: 'Charmander',
-    image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png'
-  }
+    name: "Charmander",
+    image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
+  };
 
   return (
     <>
-    <CreatePokemonCard pokemon={pokemonTest}/>
+      <CreatePokemon/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
