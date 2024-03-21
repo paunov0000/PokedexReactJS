89427252e93a67c692b454c3c953904028fd5740
paunov0000 <legendarygriefer96@gmail.com>
2024-03-21 20:49:05 +0200

    Made it fetch the correct data from the API so image is displayed as well, added leading zeroes on the id, styled the cards, needs further improvement but its ok for now

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -7,19 +7,34 @@ function CreatePokemon() {
     fetch("https://pokeapi.co/api/v2/pokemon?limit=10")
       .then((response) => response.json())
       .then((data) => {
-        console.log(data.results);
-        setPokemonList(data.results);
+        // console.log(data.results);
+        const pokemonData = data.results.map((pokemon) => {
+          return fetch(pokemon.url)
+            .then((response) => response.json())
+            .then((data) => {
+              return {
+                id: data.id.toString().padStart(3, "0"),
+                name: data.name,
+                image: data.sprites.front_default,
+              };
+            });
+        })
+
+        Promise.all(pokemonData).then((pokemonData) => {
+          setPokemonList(pokemonData);
+        });
+        // setPokemonList(pokemonData);
       });
   }, []);
 
   return (
-    <div>
+    <div className="grid grid-cols-3 gap-6">
       {pokemonList.map((pokemon) => {
         return (
-          <div className="flex flex-col items-center w-28 p-3 border-solid border-2">
-            <div>
+          <div className="flex flex-col items-center w-36 p-3 border-solid border-2 rounded-2xl">
+            <div className="flex flex-col items-center">
               <p className="w-full text-right">#{pokemon.id}</p>
-              <div className="w-14 flex">
+              <div className="w-28 flex">
                 <img
                   className="w-full"
                   src={pokemon.image}
