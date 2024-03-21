16c5d18f357a7f8cd93b00bf3aec41edc08c2911
paunov0000 <legendarygriefer96@gmail.com>
2024-03-22 00:20:09 +0200

    added the CreateNavbar component, and most importantly impleneted the fetching here so I can await it and therefore not display CreateNavbar before CreatePokemon has fetched its' data

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,45 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import CreatePokemon from "./components/Pokemon.jsx";
+import CreateNavbar from "./components/Navbar.jsx";
 
 function App() {
-  // const [count, setCount] = useState(0)
-  const pokemonTest = {
-    id: 4,
-    name: "Charmander",
-    image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
-  };
+  const [pokemonArr, setPokemonList] = useState(null);
+
+  useEffect(() => {
+    fetchData().then((pokemonList) => {
+      setPokemonList(pokemonList);
+    });
+  }, []);
+
+  if (!pokemonArr) {
+    return <h1>Loading...</h1>;
+  }
+
 
   return (
     <>
-      <CreatePokemon/>
+      <CreateNavbar/>
+      <CreatePokemon pokemonArr={pokemonArr} />
     </>
   );
+}
+
+async function fetchData() {
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
+  const data = await response.json();
+  const pokemonData = data.results.map(async (pokemon) => {
+    const response = await fetch(pokemon.url);
+    const data = await response.json();
+    return {
+      id: data.id.toString().padStart(3, "0"),
+      name: data.name.charAt(0).toUpperCase() + data.name.slice(1),
+      image: data.sprites.front_default,
+    };
+  });
+
+  const pokemonList = await Promise.all(pokemonData);
+  return pokemonList;
 }
 
 export default App;
